be9f118a3478f23aa49d00bc696f13c5c5254ccb
Peter verhaar <peter_Verhaar@live.nl>
2023-07-01 15:49:18 +0200

    *

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Azur Lane Tier List</title>
 
-    @vite(['resources/css/main.css', 'resources/js/main.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		laravel({
-			input: ['resources/css/main.css', 'resources/js/main.js'],
+			input: ['resources/css/app.css', 'resources/js/app.js'],
 			refresh: true,
 		}),
 	],
